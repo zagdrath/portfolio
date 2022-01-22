0588f456dda17488ad395aed4aea389ded766073
Cody Wellman <zagdrath@member.fsf.org>
2022-01-22 10:41:51 -0500

    Add Gatsby code

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+// Imports
+import * as React from 'react'
+import { Helmet } from 'react-helmet';
+
+// Define component
+const IndexPage = () => {
+  return (
+    <body>
+      <Helmet>
+        <title>Zagdrath</title>
+        <meta charset="UTF-8"></meta>
+      </Helmet>
+      
+    </body>
+  )
+}
+
+// Export component
+export default IndexPage
